726b42022637d88424a43a57ff167677e357a57d
Mikhail Larionov <larionov404.mu@gmail.com>
2024-10-08 14:06:44 +0300

    создание адаптива

--- frontend/src/components/Artifacts/artifacts.tsx
+++ frontend/src/components/Artifacts/artifacts.tsx
@@ -6,24 +6,35 @@ import styles from "./artifacts.module.scss";
 
 export default function New() {
   return (
-    <div className={`${styles["new"]} container mt-[60px]`}>
+    <div className={`${styles["new"]} container mt-[60px] px-[10px]`}>
       <h1 className={`${styles["new_title"]} text-[48px] font-bold`}>
         Новые артефакты
       </h1>
       <div
-        className={`${styles["new__artifacts"]} flex flex-row container gap-[30px] h-[493px] mt-[30px]`}
+        className={`${styles["new__artifacts"]} flex flex-row container gap-[30px] mt-[30px]`}
       >
-        <Image src={artifacts} alt="artifacts" />
-        <div className={`${styles["new__artifacts_info"]} bg-[#bae249] p-[16px]`}>
-          <h1 className={`${styles["new__info_title"]} text-[#1D2F30] text-[24px] font-bold`}>Kurische Nehrung 24</h1>
-          <p className={`${styles["new__info_description"]} text-[#506829] text-[16px] mt-[8px]`}>
+        <div className="flex-[0_0_75%]">
+          <Image src={artifacts} alt="artifacts" />
+        </div>
+        <div
+          className={`${styles["new__artifacts_info"]} bg-[#bae249] p-[16px] `}
+        >
+          <h1
+            className={`${styles["new__info_title"]} text-[#1D2F30] text-[24px] font-bold`}
+          >
+            Kurische Nehrung 24
+          </h1>
+          <p
+            className={`${styles["new__info_description"]} text-[#506829] text-[16px] mt-[8px]`}
+          >
             Вот вам яркий пример современных тенденций - начало повседневной
             работы по формированию позиции выявляет срочную потребность методов
             управления процессами.
-            <br/><br/>
-            Есть над чем задуматься: представители
-            современных социальных резервов своевременно верифицированы.Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit.
+            <br />
+            <br />
+            Есть над чем задуматься: представители современных социальных
+            резервов своевременно верифицированы.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit.
           </p>
         </div>
       </div>
--- frontend/src/components/Cover/cover.tsx
+++ frontend/src/components/Cover/cover.tsx
@@ -3,13 +3,17 @@ import styles from "./cover.module.scss";
 export default function Cover() {
   return (
     <div className={styles["cover"]}>
-      <div className={styles["cover__description"]}>
-        <h1>
+      <div className={`${styles["cover__description"]} px-[20px]`}>
+        <h1
+          className={`${styles["cover__description_title"]} font-bold`}
+        >
           Крупнейшая коллекция
           <br />
           природных артефактов
         </h1>
-        <div>
+        <div
+          className={`${styles["cover__description_text"]} mt-[16px] text-[16px]`}
+        >
           Являясь всего лишь частью общей картины, интерактивные прототипы
           <br />
           которые представляют собой яркий пример европейского типа политической
